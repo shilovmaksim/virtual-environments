9ad412ffa6c557bbf203e9320239fa1ba83ca66f
Maksim Shilov <maksim.shilov@akvelon.com>
2021-11-24 12:51:09 +0300

    Do not import anything in software report

--- images/macos/software-report/SoftwareReport.Generator.ps1
+++ images/macos/software-report/SoftwareReport.Generator.ps1
@@ -4,23 +4,6 @@ param (
     $ImageName
 )
 
-$ErrorActionPreference = "Stop"
-
-Import-Module MarkdownPS
-Import-Module "$PSScriptRoot/SoftwareReport.Common.psm1" -DisableNameChecking
-Import-Module "$PSScriptRoot/SoftwareReport.Xcode.psm1" -DisableNameChecking
-Import-Module "$PSScriptRoot/SoftwareReport.Android.psm1" -DisableNameChecking
-Import-Module "$PSScriptRoot/SoftwareReport.Java.psm1" -DisableNameChecking
-Import-Module "$PSScriptRoot/SoftwareReport.Xamarin.psm1" -DisableNameChecking
-Import-Module "$PSScriptRoot/SoftwareReport.Toolcache.psm1" -DisableNameChecking
-Import-Module "$PSScriptRoot/SoftwareReport.Browsers.psm1" -DisableNameChecking
-Import-Module "$PSScriptRoot/SoftwareReport.WebServers.psm1" -DisableNameChecking
-Import-Module "$PSScriptRoot/../helpers/SoftwareReport.Helpers.psm1"
-Import-Module "$PSScriptRoot/../helpers/Common.Helpers.psm1"
-Import-Module "$PSScriptRoot/../helpers/Xcode.Helpers.psm1"
-
-
-
 if (-not (Test-Path $OutputDirectory)) { New-Item -Path $OutputDirectory -ItemType Directory | Out-Null }
 
 #
